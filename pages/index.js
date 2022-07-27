88d329a62b8bb6387b2a0e6f62a11d18d20986f6
mujeex <mujahid.bappai@yahoo.com>
2022-07-27 21:11:37 +0100

    init: initialize application with create-customer form

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,27 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import {useState} from 'react'
 import styles from '../styles/Home.module.css'
+import {Button,Modal,Form,Input} from 'antd'
+import 'antd/dist/antd.css';
 
 export default function Home() {
+
+  const [isModalOpen,setIsModalOpen] = useState(false);
+  const [customerList, setCustomerList] =  useState([]);
+
+  function toggleModal(){
+    setIsModalOpen(!isModalOpen);
+  }
+
+  function handleSubmitCustomer(customerData){
+    console.log(customerData)
+
+    // save to database
+    // save to local state
+    // close modal
+    toggleModal();
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,58 +31,61 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+       <Button onClick={toggleModal}>Create customer</Button>
+        <CustomerModal isModalOpen={isModalOpen} onToggleModal={toggleModal} handleSubmitCustomer={handleSubmitCustomer}/>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
+  )
+}
+
+function CustomerModal({isModalOpen,onToggleModal,handleSubmitCustomer}){
+  return(
+    <Modal
+    title="Create a new customer"
+    centered
+    visible={isModalOpen}
+    onOk={onToggleModal}
+    onCancel={onToggleModal}
+    footer={null}
+    // width={1000}
+  >
+    <ModalForm onSubmitCustomer={handleSubmitCustomer}/>
+  </Modal>
+  )
+}
+
+
+function ModalForm({onSubmitCustomer}){
+  return(
+    <Form
+      layout='vertical'
+      name="customerForm"
+      initialValues={{ remember: true }}
+      onFinish={onSubmitCustomer}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="customerName"
+        name="Customer Name"
+        rules={[{ required: true, message: 'Please input name of customer!' }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        name="phone"
+        label="Phone Number"
+        rules={[{ required: true, message: 'Please input customer phone number!' }]}
+      >
+        <Input addonBefore={'+234'} style={{ width: '100%' }} />
+      </Form.Item>
+
+
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
   )
 }
